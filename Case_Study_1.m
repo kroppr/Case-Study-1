711558e602d3d5f9ff4a3c96c18798f5237b3dbb
rwalsh7231 <112423655+rwalsh7231@users.noreply.github.com>
2022-09-28 17:05:15 -0500

    Add files via upload
    Latest Update

--- Case_Study_1.m
+++ Case_Study_1.m
@@ -7,3 +7,14 @@ COVIDbyCounty = load("COVIDbyCounty.mat");
 %deal it out to each variable
 [CNTY_CENSUS, CNTY_COVID, dates, divisionLabels, divisionNames] = deal(COVIDbyCounty.CNTY_CENSUS, COVIDbyCounty.CNTY_COVID, COVIDbyCounty.dates, COVIDbyCounty.divisionLabels, COVIDbyCounty.divisionNames);
 
+%With the data defined, we need to cluster it, I think a K of 9 fits best
+%as there are 9 major sections of the United States
+
+clustered = kmeans(CNTY_COVID, 9, 'Replicates', 25);
+
+%make a silhouette plot to see how good we are doing
+silhouette(CNTY_COVID, clustered)
+
+values = silhouette(CNTY_COVID, clustered);
+
+
