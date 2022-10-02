7382f88066b2c2190eb6581b85e6bfa8975b517b
kroppr <112423749+kroppr@users.noreply.github.com>
2022-10-02 16:40:33 -0500

    Add files via upload

--- Case_Study.m
+++ Case_Study.m
@@ -18,21 +18,6 @@ while i < size(CNTY_COVID, 1)
 end
 
 
-collect2 = zeros(225,5);
-i = 0;
-while i < size(CNTY_COVID, 1)
-   j = 0;
-   i = i + 1;
-   val2 = maxk(CNTY_COVID(i,:), 4);
-   while j < size(CNTY_COVID, 2)
-       j = j + 1;
-       if CNTY_COVID(i,j) == val
-           collect(i,:) = [i j];
-       end
-   end
-end
-
-
 
 %%      PLOT STUFF
 
@@ -51,6 +36,70 @@ while n < 10
     end
     n = n + 1;
 end
+
+
+
+%%
+
+% region1 = CNTY_COVID(1:25,:);
+% ave1 = mean(region1,1);
+% region2 = CNTY_COVID(26:50,:);
+% ave2 = mean(region2,1);
+% region3 = CNTY_COVID(51:75,:);
+% ave3 = mean(region3,1);
+% region4 = CNTY_COVID(76:100,:);
+% ave4 = mean(region4,1);
+% region5 = CNTY_COVID(101:125,:);
+% ave5 = mean(region5,1);
+% region6 = CNTY_COVID(126:150,:);
+% ave6 = mean(region6,1);
+% region7 = CNTY_COVID(151:175,:);
+% ave7 = mean(region7,1);
+% region8 = CNTY_COVID(176:200,:);
+% ave8 = mean(region8,1);
+% region9 = CNTY_COVID(201:225,:);
+% ave9 = mean(region9,1);
+
+
+i = 1;
+maxes = [];
+while i <= sz
+    arr = CNTY_COVID(i,:);
+    m = max(arr);
+    idx = find(CNTY_COVID(i,:) == m,1);
+    maxes(end + 1) = idx;
+    i = i + 1;
+    if rem(i,26) == 0
+        disp(mean(maxes))
+        maxes = [];
+    end
+end
+disp(mean(maxes))
+
+
+% figure(10);
+% plot(ave1);
+% hold on
+% plot(ave2);
+% plot(ave3);
+% plot(ave4);
+% plot(ave5);
+% plot(ave6);
+% plot(ave7);
+% plot(ave8);
+% plot(ave9);
+% legend("1","2","3","4","5","6","7","8","9");
+% hold off
+
+%%
+
+
+scale = 100;
+
+table = cat(1,ave1,ave2,ave3,ave4,ave5,ave6,ave7,ave8,ave9);
+
+
+
 
 
 
