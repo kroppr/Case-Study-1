6d65e394b48f8cc974a6138e57fb752245678ebc
rwalsh7231 <112423655+rwalsh7231@users.noreply.github.com>
2022-10-03 11:27:40 -0500

    Add files via upload
    A Perfect Cluster? How can we incorporate it for an unlabeled set?

--- Case_Study.m
+++ Case_Study.m
@@ -1,8 +1,10 @@
 %% First file
 
+%load and deal data
 data = load('COVIDbyCounty.mat');
 [dates, CNTY_CENSUS,CNTY_COVID, divisionLabels, divisionNames] = deal(data.dates, data.CNTY_CENSUS, data.CNTY_COVID, data.divisionLabels, data.divisionNames);
 
+%Create an array with the max of each row
 collect = zeros(225,2);
 i = 0;
 while i < size(CNTY_COVID, 1)
@@ -22,6 +24,8 @@ end
 %%      PLOT STUFF
 
 n = 1;
+
+%Run 9 times
 while n < 10
     figure(n);                          %New figure for each iteration
     i = 1;
@@ -41,29 +45,31 @@ end
 
 %%
 
-% region1 = CNTY_COVID(1:25,:);
-% ave1 = mean(region1,1);
-% region2 = CNTY_COVID(26:50,:);
-% ave2 = mean(region2,1);
-% region3 = CNTY_COVID(51:75,:);
-% ave3 = mean(region3,1);
-% region4 = CNTY_COVID(76:100,:);
-% ave4 = mean(region4,1);
-% region5 = CNTY_COVID(101:125,:);
-% ave5 = mean(region5,1);
-% region6 = CNTY_COVID(126:150,:);
-% ave6 = mean(region6,1);
-% region7 = CNTY_COVID(151:175,:);
-% ave7 = mean(region7,1);
-% region8 = CNTY_COVID(176:200,:);
-% ave8 = mean(region8,1);
-% region9 = CNTY_COVID(201:225,:);
-% ave9 = mean(region9,1);
+region1 = CNTY_COVID(1:25,:);
+ave1 = mean(region1,1);
+region2 = CNTY_COVID(26:50,:);
+ave2 = mean(region2,1);
+region3 = CNTY_COVID(51:75,:);
+ave3 = mean(region3,1);
+region4 = CNTY_COVID(76:100,:);
+ave4 = mean(region4,1);
+region5 = CNTY_COVID(101:125,:);
+ave5 = mean(region5,1);
+region6 = CNTY_COVID(126:150,:);
+ave6 = mean(region6,1);
+region7 = CNTY_COVID(151:175,:);
+ave7 = mean(region7,1);
+region8 = CNTY_COVID(176:200,:);
+ave8 = mean(region8,1);
+region9 = CNTY_COVID(201:225,:);
+ave9 = mean(region9,1);
 
 
 i = 1;
 maxes = [];
-while i <= sz
+%What is sz?
+%while i <= sz
+while i <= height(CNTY_COVID)
     arr = CNTY_COVID(i,:);
     m = max(arr);
     idx = find(CNTY_COVID(i,:) == m,1);
@@ -77,31 +83,81 @@ end
 disp(mean(maxes))
 
 
-% figure(10);
-% plot(ave1);
-% hold on
-% plot(ave2);
-% plot(ave3);
-% plot(ave4);
-% plot(ave5);
-% plot(ave6);
-% plot(ave7);
-% plot(ave8);
-% plot(ave9);
-% legend("1","2","3","4","5","6","7","8","9");
-% hold off
+figure(10);
+plot(ave1);
+hold on
+plot(ave2);
+plot(ave3);
+plot(ave4);
+plot(ave5);
+plot(ave6);
+plot(ave7);
+plot(ave8);
+plot(ave9);
+legend("1","2","3","4","5","6","7","8","9");
+xlabel("Days")
+title("Average Data")
+hold off
 
 %%
 
 
 scale = 100;
 
-table = cat(1,ave1,ave2,ave3,ave4,ave5,ave6,ave7,ave8,ave9);
+table = cat(1, ave1,ave2,ave3,ave4,ave5,ave6,ave7,ave8,ave9);
+
+figure()
+
+for i =  1:height(table)
+    hold on
+    plot(table(i,:))
+
+end
+axis tight
+
+%% CLUSTER
+
+% Experiments to get an accurate cluster value
+E1 = (CNTY_COVID(1:25, :)./ ave1)+ave1;
+E2 = (CNTY_COVID(26:50, :)./ ave2)+ave2;
+E3 = (CNTY_COVID(51:75, :)./ ave3)+ave3;
+E4 = (CNTY_COVID(76:100, :)./ ave4)+ave4;
+E5 = (CNTY_COVID(101:125, :)./ ave5)+ave5;
+E6 = (CNTY_COVID(126:150, :)./ ave6)+ave6;
+E7 = (CNTY_COVID(151:175, :)./ ave7)+ave7;
+E8 = (CNTY_COVID(176:200, :)./ ave8)+ave8;
+E9 = (CNTY_COVID(201:225, :)./ ave9)+ave9;
+
+result = cat(1, E1, E2, E3, E4, E5, E6, E7, E8, E9);
+
+cluster = kmeans(result, 9);
+
+%In doing this I managed to get a perfect cluster. I wonder if I could try
+%this without this method
+
+%get the total differences between all the columns
+for i = 1:width(CNTY_COVID)-1
+    temp(:,i) = (CNTY_COVID(:,i+1)-CNTY_COVID(:,i));
+end
+
+avg2 = mean(temp, 2)/129;
+
+E12 = (temp./avg2) + avg2;
+
+avg = mean(CNTY_COVID,2);
+E10 = (((CNTY_COVID) ./ avg)+avg);
+E10 = mean(E10, 2);
+
+E11 = ((temp/130));
+
+cluster2 = kmeans(E10, 9, 'Replicates', 50);
+
+cluster3 = kmeans(E12, 9, 'Replicates', 50);
+
+%accuracy calculation
+for i = 1:9
 
 
-
-
-
-
+end
 
 
