ef820b6345f4289c73bf2bedd236fc5c3d28f123
matthewrwalker <114446259+matthewrwalker@users.noreply.github.com>
2022-10-31 23:00:34 -0500

    Add files via upload

--- Case Study 2 Stuff/siroutput.m
+++ Case Study 2 Stuff/siroutput.m
@@ -29,7 +29,7 @@ x0 = [ic_susc;ic_inf;ic_rec;ic_fatality];
 
 % simulate the SIRD model for t time-steps
 sys_sir_base = ss(A,B,eye(4),zeros(4,1),1);
-y = lsim(sys_sir_base,zeros(t,1),linspace(0,t-1,t),x0);
+y = lsim(sys_sir_base,zeros(t,1),linsp ace(0,t-1,t),x0);
 
 % return a "cost".  This is the quantitity that you want your model to
 % minimize.  Basically, this should encapsulate the difference between your
@@ -39,7 +39,6 @@ y = lsim(sys_sir_base,zeros(t,1),linspace(0,t-1,t),x0);
 
 %We want actual and modeled cases, along with actual and modeled
 %deaths.
-
 
 
 STLmetroPopFixed=27.3714*100000;
