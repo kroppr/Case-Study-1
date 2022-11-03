7a4bf0c32518ad6ffbf2d7caa18c0219ea4358e0
rwalsh7231 <112423655+rwalsh7231@users.noreply.github.com>
2022-11-02 21:02:47 -0500

    Add files via upload
    uploaded to folder

--- Case Study 2 Stuff/base_sir_fit.m
+++ Case Study 2 Stuff/base_sir_fit.m
@@ -1,3 +1,8 @@
+
+data = load("COVIDdata.mat");
+COVID_STLmetro = deal(data.COVID_STLmetro);
+STLmetroPopFixed=27.3714*100000;
+
 COVID_MO_array=table2array(COVID_STLmetro(:,5:6));
 COVID_MO_proportion=COVID_MO_array/STLmetroPopFixed;
 
@@ -17,7 +22,7 @@ sirafun= @(x)siroutput(x,t,coviddata);
 % Set A and b to impose a parameter inequality constraint of the form A*x < b
 % Note that this is imposed element-wise
 % If you don't want such a constraint, keep these matrices empty.
-A = [0; 0; 0; .99; .99; .99; .99;];
+A = [0 0 0 .99 0.99 0.99 0.99];
 b = [1];
 
 %% set up some fixed constraints
@@ -37,11 +42,11 @@ ub = [1]';
 lb = [0]';
 
 % Specify some initial parameters for the optimizer to start from
-x0 = [1 0 0 0]; 
+x0 = [0 0 0 1 0 0 0]; 
 
 % This is the key line that tries to opimize your model parameters in order to
 % fit the data
-% note tath you 
+% note that you 
 x = fmincon(sirafun,x0,A,b,Af,bf,lb,ub);
 
 %plot(Y);
