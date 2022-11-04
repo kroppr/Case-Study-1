9b3146a8759528249d6a601652b199fabe4bfd02
rwalsh7231 <112423655+rwalsh7231@users.noreply.github.com>
2022-11-04 09:49:41 -0500

    Add files via upload
    Significant changes, model for each segment is functional. Infection rate is nearly perfect, but little inconsistencies cause the death rate to be inaccurate

--- Case Study 2 Stuff/base_sir_fit.m
+++ Case Study 2 Stuff/base_sir_fit.m
@@ -50,6 +50,7 @@ x0 = [0 0 0 1 0 0 0];
 x = fmincon(sirafun,x0,A,b,Af,bf,lb,ub);
 
 
+%% Model each segment
 %Segments to focus on:
 %0-100, 101-250, 251-350, 351-500, 500-650, 651-700, 701-end
 
@@ -72,23 +73,68 @@ params2 = [0 0 0 New1(1) New1(2) New1(3) New1(4)];
 x2 = fmincon(fun2, params2, A, b, Af, bf, lb, ub);
 Y_fit_2 = siroutput_full(x2, 150);
 
+New2(1) = Y_fit_2(150, 1);
+New2(2) = Y_fit_2(150, 2);
+New2(3) = Y_fit_2(150, 3);
+New2(4) = Y_fit_2(150, 4);
+
+
 segment_3 = coviddata(251:350,:);
 fun3= @(x)siroutput(x,100,segment_3);
+params3 = [0 0 0 New2(1) New2(2) New2(3) New2(4)];
+x3 = fmincon(fun3, params3, A, b, Af, bf, lb, ub);
+Y_fit_3 = siroutput_full(x3, 100);
+
+New3(1) = Y_fit_3(100, 1);
+New3(2) = Y_fit_3(100, 2);
+New3(3) = Y_fit_3(100, 3);
+New3(4) = Y_fit_3(100, 4);
+
 
 segment_4 = coviddata(351:500,:);
 fun4= @(x)siroutput(x,150,segment_4);
+params4 = [0 0 0 New3(1) New3(2) New3(3) New3(4)];
+x4 = fmincon(fun4, params4, A, b, Af, bf, lb, ub);
+Y_fit_4 = siroutput_full(x4, 150);
+
+New4(1) = Y_fit_4(150, 1);
+New4(2) = Y_fit_4(150, 2);
+New4(3) = Y_fit_4(150, 3);
+New4(4) = Y_fit_4(150, 4);
+
 
 segment_5 = coviddata(501:650,:);
 fun5= @(x)siroutput(x,150,segment_5);
+params5 = [0 0 0 New4(1) New4(2) New4(3) New4(4)];
+x5 = fmincon(fun5, params5, A, b, Af, bf, lb, ub);
+Y_fit_5 = siroutput_full(x5, 150);
+
+New5(1) = Y_fit_5(150, 1);
+New5(2) = Y_fit_5(150, 2);
+New5(3) = Y_fit_5(150, 3);
+New5(4) = Y_fit_5(150, 4);
 
 segment_6 = coviddata(651:700,:);
 fun6= @(x)siroutput(x,50,segment_6);
+params6 = [0 0 0 New5(1) New5(2) New5(3) New5(4)];
+x6 = fmincon(fun6, params6, A, b, Af, bf, lb, ub);
+Y_fit_6 = siroutput_full(x6, 50);
+
+New6(1) = Y_fit_6(50, 1);
+New6(2) = Y_fit_6(50, 2);
+New6(3) = Y_fit_6(50, 3);
+New6(4) = Y_fit_6(50, 4);
 
 segment_7 = coviddata(701:798,:);
 fun7= @(x)siroutput(x,t-700,segment_7);
+params7 = [0 0 0 New6(1) New6(2) New6(3) New6(4)];
+x7 = fmincon(fun7, params7, A, b, Af, bf, lb, ub);
+Y_fit_7 = siroutput_full(x7, t-700);
+
+Segmented_Fit = cat(1, Y_fit_1, Y_fit_2, Y_fit_3, Y_fit_4, Y_fit_5, Y_fit_6, Y_fit_7);
 
 
-%% SEGMENTS OF BASE MODEL
+%% SEGMENTS OF BASE MODEL, ignore this
 
 %Adjust first segment of pandemic
 x_new_1 = x;
@@ -191,6 +237,7 @@ Y_fit_new = cat(1, Y_fit_A, Y_fit_B, Y_fit_C, Y_fit_D, Y_fit_E, Y_fit_F, Y_fit_G
 
 %Y_fit_new=cat(1,Y_fit_A,Y_fit_B);
 
+%% Plotting
 figure(1);
 hold on
 plot(COVID_MO_proportion(:,2));
@@ -217,7 +264,8 @@ hold on
 plot(COVID_MO_proportion(:,1));
 plot(1-Y_fit(:,1));
 plot(1-Y_fit_new(:,1));
-legend('Actual Cases','Modeled Cases', 'New Modeled Cases');
+plot(1 - Segmented_Fit(:,1));
+legend('Actual Cases','Modeled Cases', 'New Modeled Cases', 'Modified Model Cases');
 xlabel('Time')
 title('Proportion of Population Infected')
 hold off
@@ -227,6 +275,7 @@ hold on
 plot(COVID_MO_proportion(:,2));
 plot(Y_fit(:,4));
 plot(Y_fit_new(:,4));
+plot(Segmented_Fit(:,4));
 legend('Actual Deaths','Modeled Deaths', 'New Modeled Deaths');
 xlabel('Time')
 title('Proportion of Population Deceased')
