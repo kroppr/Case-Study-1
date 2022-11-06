04bd2b230bf1ebaa0f14b0e16c6d0ebc1df62cca
kroppr <112423749+kroppr@users.noreply.github.com>
2022-11-06 13:47:24 -0600

    Add files via upload

--- Case Study 2 Stuff/comp.m
+++ Case Study 2 Stuff/comp.m
@@ -15,88 +15,86 @@ plot(infected);
 
 A = [0 0 0 .99 .99 .99 .99];
 b = 1;
-Af = [0 0 0 1 1 1 1];                           % Copied from base_sir_fit. Don't think parameters causing issue.
+Af = [0 0 0 1 1 1 1];                           
 bf = 1;
 ub = [1 1 1 1 1 1 1]';
 lb = [0 0 0 0 0 0 0]';
+x0 = [0 0 0 1 0 0 0];
 
 %% First Segment: Days 1-100
 t = 100;
+seg1 = data(1:100,:);
+                 
+fun1= @(x1)siroutput(x1,t,seg1);      
+x1 = fmincon(fun1,x0,A,b,Af,bf,lb,ub); 
+Y_fit_1 = siroutput_full(x1,t);
 
-x0 = [0 0 0 .998 .001 0 .001];                  % Set x0 to day 1 percentages, roughly. Don't think this is necessary
-sirafun= @(x)siroutput(x,t,data(1:100,:));      % Function to optimize over first 100 days
-x = fmincon(sirafun,x0,A,b,Af,bf,lb,ub);        % Optimize it
+New1(1) = Y_fit_1(100, 1);
+New1(2) = Y_fit_1(100, 2);
+New1(3) = Y_fit_1(100, 3);
+New1(4) = Y_fit_1(100, 4);        
 
-x_next1 = x;                                    % First 'x' is good to plug into siroutput_full
-Y_fit_1 = siroutput_full(x_next1,t);            % Get the fit
+%% Second Segment: Days 101-125
+t = 25;                                         
 
-% In this segment and the others, copied Matt's procedure to generate the best
-% fit.
+seg2 = data(101:125, :);
+fun2= @(x2)siroutput(x2,t,seg2);
+params2 = [x1(1) x1(2) x1(3) New1(1) New1(2) New1(3) New1(4)];
+x2 = fmincon(fun2, params2, A, b, Af, bf, lb, ub);
+Y_fit_2 = siroutput_full(x2, t);    
 
-%% Second Segment
-t = 25;                                         % Now getting next 25 days, where first "spike" rises.
+New2(1) = Y_fit_2(25, 1);
+New2(2) = Y_fit_2(25, 2);
+New2(3) = Y_fit_2(25, 3);
+New2(4) = Y_fit_2(25, 4); 
 
-x0 = [0 0 0 .956 .004 0 0.04];                  % Day 100 percentages. Again, pretty sure this unecessary.
-sirafun= @(x)siroutput(x,t,data(101:125,:));    % New function for next 25 days
-x = fmincon(sirafun,x0,A,b,Af,bf,lb,ub);        % Optimize this segment
+%% Third Segment: Days 126-199
+t = 74;                                         % Next 50 days: descent from peak
 
-x_next2 = x;                                    % Get k values from optimized x
-x_next2(4) = Y_fit_1(100,1);                    % Set ic values so they plot is continuous
-x_next2(5) = Y_fit_1(100,2);
-x_next2(6) = Y_fit_1(100,3);
-x_next2(7) = Y_fit_1(100,4);
-Y_fit_2 = siroutput_full(x_next2,t);            % Get the fit
+seg3 = data(126:199, :);
+fun3= @(x3)siroutput(x3,t,seg3);
+params3 = [x2(1) x2(2) x2(3) New2(1) New2(2) New2(3) New2(4)];
+x3 = fmincon(fun3, params3, A, b, Af, bf, lb, ub);
+Y_fit_3 = siroutput_full(x3, t);    
 
-%% Third Segment
-t = 50;                                         % Next 50 days: descent from peak
+New3(1) = Y_fit_3(50, 1);
+New3(2) = Y_fit_3(50, 2);
+New3(3) = Y_fit_3(50, 3);
+New3(4) = Y_fit_3(50, 4); 
 
-x0 = [0 0 0 .93 .02 0 .05];
-sirafun= @(x)siroutput(x,t,data(126:175,:));
-x = fmincon(sirafun,x0,A,b,Af,bf,lb,ub);
+%% Fourth Segment: Days 201-365
+t = 365-201;                                         % Next 50 days: descent from peak
 
-x_next3 = x;
-x_next3(4) = Y_fit_2(25,1);
-x_next3(5) = Y_fit_2(25,2);                     % This stuff all same as second segment
-x_next3(6) = Y_fit_2(25,3);
-x_next3(7) = Y_fit_2(25,4);
-Y_fit_3 = siroutput_full(x_next3,t);
-
-Y_fit = cat(1,Y_fit_1,Y_fit_2,Y_fit_3);         % Put 3 fits together
-
-plot(Y_fit(:,2));                               % Plot the stuff
-plot(Y_fit(:,4));
-legend('deaths','infected','minfections','mdeaths');
-title('Plot with first three segment fits')
-hold off;
+seg4 = data(202:365, :);
+fun4= @(x4)siroutput(x4,t,seg4);
+params4 = [x3(1) x3(2) x3(3) New3(1) New3(2) New3(3) New3(4)];
+x4 = fmincon(fun4, params4, A, b, Af, bf, lb, ub);
+Y_fit_4 = siroutput_full(x4, t);    
 
 
-%% Copy base_sir_fit--still gives bad fit
+%% Plot
+Y_fit = cat(1,Y_fit_1,Y_fit_2,Y_fit_3,Y_fit_4);         % Put 3 fits together
 
-t = 365;
-
-figure(2);
-hold on;
-plot(deaths);                                   % Plot actual data
-plot(infected);
-
-sirafun= @(x)siroutput(x,t,data);
-
-A = [0 0 0 .99 .99 .99 .99];
-b = 1;
-Af = [0 0 0 1 1 1 1];
-bf = 1;
-ub = [1 1 1 1 1 1 1]';
-lb = [0 0 0 0 0 0 0]';
-
-x0 = [0 0 0 1 0 0 0]; 
-
-x = fmincon(sirafun,x0,A,b,Af,bf,lb,ub);
-Y_fit = siroutput_full(x,t);
-
-plot(infected);
 plot(Y_fit(:,2));
-plot(deaths);
 plot(Y_fit(:,4));
 
-title('Plot with model format copied from base sir fit')
+legend('deaths','infected','minfections','mdeaths');
+title('Plot With ')
 hold off;
+
+%% Figuring vaccinated and breakthrough
+
+% Infections spike at vaccination time; call pre-vax flat percentage the standard
+% infection rate. The spike appears to be directly caused by vaccinations,
+% consider entire spike to be vaccinated people. INITIALLY: spike % -
+% standard % = vaccinated %. THEN: vaxed individuals 4% as likely to
+% contract, so the % infected after spike is [standard % * (1-vaxed) + 4% *
+% standard % * vaxed.
+
+StadardInfRate = sum(Y_fit(1:100,2))/100;
+
+PreVax = zeros(100);
+SpikeVax = Y_fit_2(:,2) - ones * StadardInfRate;
+PostSpikeVax = ones*StadardInfRate - Y_fit_4(:,2);
+
+
