732f7a68f205e81d50b8234ec0abc87097262d1d
rwalsh7231 <112423655+rwalsh7231@users.noreply.github.com>
2022-11-06 12:01:31 -0600

    Add files via upload
    Modified to have usable params, and applied a 25% reduction to both infections and deaths for a separate parallel dataset to the original segmented models. This parallel set should be equal to the original, with just the 25% reduction

--- Case Study 2 Stuff/base_sir_fit.m
+++ Case Study 2 Stuff/base_sir_fit.m
@@ -83,36 +83,37 @@ Y_fit_1 = siroutput_full(x1, 100);
 %model section for segment 2 using new parameters
 segment_2 = coviddata(101:250, :);
 fun2= @(x2)siroutput(x2,150,segment_2);
-x2 = fmincon(fun2, x0, A, b, Af, bf, lb, ub);
+params2 = [x1(1), x1(2), x1(3), Y_fit_1(100,1), Y_fit_1(100, 2), Y_fit_1(100, 3), Y_fit_1(100, 4)];
+x2 = fmincon(fun2, params2, A, b, Af, bf, lb, ub);
 Y_fit_2 = siroutput_full(x2, 150);
 
 segment_3 = coviddata(251:350,:);
 fun3= @(x)siroutput(x,100,segment_3);
-params3 = [x2(1) x2(2) x2(3) New2(1) New2(2) New2(3) New2(4)];
+params3 = [x2(1) x2(2) x2(3) Y_fit_2(150, 1) Y_fit_2(150, 2) Y_fit_2(150, 3) Y_fit_2(150, 4)];
 x3 = fmincon(fun3, params3, A, b, Af, bf, lb, ub);
 Y_fit_3 = siroutput_full(x3, 100);
 
 segment_4 = coviddata(351:500,:);
 fun4= @(x)siroutput(x,150,segment_4);
-params4 = [x3(1) x3(2) x3(3) New3(1) New3(2) New3(3) New3(4)];
+params4 = [x3(1) x3(2) x3(3) Y_fit_3(100, 1) Y_fit_3(100, 2) Y_fit_3(100, 3) Y_fit_3(100, 4)];
 x4 = fmincon(fun4, params4, A, b, Af, bf, lb, ub);
 Y_fit_4 = siroutput_full(x4, 150);
 
 segment_5 = coviddata(501:650,:);
 fun5= @(x)siroutput(x,150,segment_5);
-params5 = [x4(1) x4(2) x4(3) New4(1) New4(2) New4(3) New4(4)];
+params5 = [x4(1) x4(2) x4(3) Y_fit_4(150, 1) Y_fit_4(150, 2) Y_fit_4(150, 3) Y_fit_4(150, 4)];
 x5 = fmincon(fun5, params5, A, b, Af, bf, lb, ub);
 Y_fit_5 = siroutput_full(x5, 150);
 
 segment_6 = coviddata(651:700,:);
 fun6= @(x)siroutput(x,50,segment_6);
-params6 = [x5(1) x5(2) x5(3) New5(1) New5(2) New5(3) New5(4)];
+params6 = [x5(1) x5(2) x5(3) Y_fit_5(150, 1) Y_fit_5(150, 2) Y_fit_5(150, 3) Y_fit_5(150, 4)];
 x6 = fmincon(fun6, params6, A, b, Af, bf, lb, ub);
 Y_fit_6 = siroutput_full(x6, 50);
 
 segment_7 = coviddata(701:798,:);
 fun7= @(x)siroutput(x,t-700,segment_7);
-params7 = [x6(1) x6(2) x6(3) New6(1) New6(2) New6(3) New6(4)];
+params7 = [x6(1) x6(2) x6(3) Y_fit_6(50, 1) Y_fit_6(50, 2) Y_fit_6(50, 3) Y_fit_6(50, 4)];
 x7 = fmincon(fun7, params7, A, b, Af, bf, lb, ub);
 Y_fit_7 = siroutput_full(x7, t-700);
 
@@ -126,51 +127,51 @@ Segmented_Fit = cat(1, Y_fit_1, Y_fit_2, Y_fit_3, Y_fit_4, Y_fit_5, Y_fit_6, Y_f
 %Adjust first segment of pandemic
 x_new_5_1 = x1;
 %Adjust infection rate
-x_new_5_1(1)= x1(1);
+x_new_5_1(1)= x1(1)*.75;
 %Adjust fatality rate
-x_new_5_1(2) = x1(2);
+x_new_5_1(2) = x1(2)*.75;
 
 %adjust second segment of pandemic
 x_new_5_2 = x2;
 %Adjust Infection Rate
-x_new_5_2(1)= x2(1);
+x_new_5_2(1)= x2(1)*.75;
 %Adjust fatalities
-x_new_5_2(2)= x2(2);
+x_new_5_2(2)= x2(2)*.75;
 
 %adjust 3rd segment of pandemic
 x_new_5_3 = x3;
 %Adjust Infection Rate
-x_new_5_3(1)= x3(1);
+x_new_5_3(1)= x3(1)*.75;
 %Adjust fatalities
-x_new_5_3(2)= x3(2);
+x_new_5_3(2)= x3(2)*.75;
 
 %adjust 4th segment of pandemic
 x_new_5_4 = x4;
 %Adjust Infection Rate
-x_new_5_4(1)= x4(1);
+x_new_5_4(1)= x4(1)*.75;
 %Adjust fatalities
-x_new_5_4(2)= x4(2);
+x_new_5_4(2)= x4(2)*.75;
 
 %adjust 5th segment of pandemic
 x_new_5_5 = x5;
 %Adjust Infection Rate
-x_new_5_5(1)= x5(1);
+x_new_5_5(1)= x5(1)*.75;
 %Adjust fatalities
-x_new_5_5(2)= x5(2);
+x_new_5_5(2)= x5(2)*.75;
 
 %adjust 6th segment of pandemic
 x_new_5_6 = x6;
 %Adjust Infection Rate
-x_new_5_6(1)= x6(1);
+x_new_5_6(1)= x6(1)*.75;
 %Adjust fatalities
-x_new_5_6(2)= x6(2);
+x_new_5_6(2)= x6(2)*.75;
 
 %adjust 7th segment of pandemic
 x_new_5_7 = x7;
 %Adjust Infection Rate
-x_new_5_7(1)= x7(1);
+x_new_5_7(1)= x7(1)*.75;
 %Adjust fatalities
-x_new_5_7(2)= x7(2);
+x_new_5_7(2)= x7(2)*.75;
 
 %plot(Y);
 %legend('S','I','R','D');
@@ -288,4 +289,3 @@ xlabel('Time')
 ylabel('Cumulative Proportion')
 title('Cumulative Proportion of Population Deceased')
 hold off
-
