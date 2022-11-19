5dcd7c4ff7287ef08d9b6ee98697a77ab0ac31c1
matthewrwalker <114446259+matthewrwalker@users.noreply.github.com>
2022-11-19 17:57:09 -0600

    Add files via upload

--- First.m
+++ First.m
@@ -44,9 +44,9 @@ hold off;
 
 %% Focus
 
-d1 = 2;
-d2 = 4;
-f = 0.05;
+d1 = 0.1;
+d2 = 0.2;
+f = 0.15;
 
 M1= [1 d1 0 0;
      0 1 0 0;
@@ -57,33 +57,51 @@ M2= [1 d2 0 0;
      0 0 1 d2;
      0 0 0 1];
 Mf= [1 0 0 0;
-     -1/f 1 0 0;
+     (-1/f) 1 0 0;
      0 0 1 0;
-     0 0 -1/f 1];
+     0 0 (-1/f) 1];
 
 rays_in0 = [0 0 0 0 0;
            10 5 0 -5 -10;
            0 0 0 0 0;
            10 5 0 -5 -10];
-rays_out10 = M1*rays_in0;
+rays_out0M1 = M1*rays_in0;
 
-rays_out20 = Mf*rays_out10;
+rays_out0Mf = Mf*rays_out0M1;
+
+rays_out0M2= M2*rays_out0Mf;
 
 rays_in1 = [10 10 10 10 10;
            10 5 0 -5 -10;
            10 10 10 10 10;
            10 5 0 -5 -10];
-rays_out11 = M1*rays_in0;
+rays_out1M1 = M1*rays_in1;
 
-rays_out21 = Mf*rays_out11;
+rays_out1Mf = Mf*rays_out1M1;
 
-rays_out10 = cat(2,rays_out10, rays_out11);
-rays_out20 = cat(2,rays_out20, rays_out21);
-ray_z = [zeros(1,size(rays_in10,2)); d*ones(1,size(rays_in10,2))];
+rays_out1M2 = M2*rays_out1Mf;
+
+ray_z1 = [zeros(1,size(rays_in0,2)); d1*ones(1,size(rays_in0,2))];
+ray_z2 = [d1*ones(1,size(rays_in0,2)); d2*ones(1,size(rays_in0,2))];
 
 figure(4)
-plot(ray_z, [rays_in0(1,:); rays_out10(1,:)], Color="r");
 hold on;
-plot(ray_z, [rays_in10(1,:); rays_out11(1,:)], Color="b");
+
+plot(ray_z1, [rays_in0(1,:); rays_out0M1(1,:)], Color="r");
+plot(ray_z1, [rays_in1(1,:); rays_out1M1(1,:)], Color="b");
+
+plot(ray_z2, [rays_out0M1(1,:); rays_out0M2(1,:)], Color="r");
+plot(ray_z2, [rays_out1M1(1,:); rays_out1M2(1,:)], Color="b");
 hold off;
 
+
+% rays_out1 = cat(2,rays_out0M1, rays_out1M1);
+% rays_out2 = cat(2,rays_out0Mf, rays_out1Mf);
+% rays_out3 = cat(2,rays_out0M2, rays_out1Mf);
+% ray_z1 = [zeros(1,size(rays_in0,2)); d1*ones(1,size(rays_in0,2))];
+% 
+% figure(4)
+%  hold on;
+% plot(ray_z, [rays_in0(1,:); rays_out0M1(1,:)], Color="r");
+% plot(ray_z, [rays_out0M1(1,:); rays_out1M1(1,:)], Color="b");
+% hold off;
